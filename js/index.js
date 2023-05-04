ab28ba876b260e02453aedebd8bda8250896275b
Simon <S.bjorkberg@gmail.com>
2023-05-04 21:51:50 +0200

    added some styling to the main screen

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const instructions = document.getElementById('instructions')
-const test = document.getElementById('instruction-container')
+const instructionContainer = document.getElementById('instruction-container')
 
 instructions.addEventListener('mouseover', () => {
-    test.style.display = 'block'
+    instructionContainer.style.display = 'block'
 })
 instructions.addEventListener('mouseout', () => {
-    test.style.display = 'none'
+    instructionContainer.style.display = 'none'
 })
