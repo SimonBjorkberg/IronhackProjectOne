37cede242c615e51f1edf23cd9cac112368f8b73
heylukechen <lukechen0114@gmail.com>
2023-05-05 23:47:31 +0200

    dino now can jump

--- js/index.js
+++ js/index.js
@@ -12,29 +12,57 @@ let dino = {
   y: 240,
   width: 40,
   height: 40,
+  speedY: 0,
+  ground: 240,
+  gravity: 0.25,
 };
-
-const ctx = canvas.getContext("2d");
 
 let currentDinoImage = dinoRight;
 let frameCount = 0;
 const frameInterval = 8;
 
 function dinoRun() {
-  ctx.clearRect(dino.x, dino.y, dino.width, dino.height);
+  context.clearRect(dino.x, dino.y, dino.width, dino.height);
   frameCount++;
   if (frameCount % frameInterval === 0) {
     if (currentDinoImage === dinoRight) {
-        currentDinoImage = dinoLeft;
+      currentDinoImage = dinoLeft;
     } else {
-        currentDinoImage = dinoRight;
+      currentDinoImage = dinoRight;
     }
   }
-  ctx.drawImage(currentDinoImage, dino.x, dino.y, dino.width, dino.height);
+  context.drawImage(currentDinoImage, dino.x, dino.y, dino.width, dino.height);
   requestAnimationFrame(dinoRun);
 }
 
+function dinoJump(e) {
+  if (e.code == "Space" && dino.y === dino.ground) {
+    dino.speedY = -6;
+  }
+}
+
+
+function updateGame() {
+  //dino
+  dino.speedY += dino.gravity;
+  dino.y = Math.min(dino.y + dino.speedY, dino.ground);
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  frameCount++;
+  if (frameCount % frameInterval === 0) {
+    if (currentDinoImage === dinoRight) {
+      currentDinoImage = dinoLeft;
+    } else {
+      currentDinoImage = dinoRight;
+    }
+  }
+  context.drawImage(currentDinoImage, dino.x, dino.y, dino.width, dino.height);
+
+  requestAnimationFrame(updateGame);
+}
+
 dinoRight.addEventListener("load", () => {
-  // Ensure to draw the image on the correct context
-  dinoRun();
+  updateGame();
 });
+
+document.addEventListener("keydown", dinoJump);
