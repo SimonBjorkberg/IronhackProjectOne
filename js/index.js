98b0e98659d40bfeb62454bf98ec98908cbdf391
Simon <S.bjorkberg@gmail.com>
2023-05-06 12:01:10 +0200

    added the small cactus

--- js/index.js
+++ js/index.js
@@ -1,36 +1,56 @@
+class Obstacle {
+  constructor(width, height, image, x, y, speed) {
+    this.width = width;
+    this.height = height;
+    this.image = new Image();
+    this.image.src = image
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+  }
+  update() {
+    this.x += -this.speed
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
+
+const smallCac = '/images/cactus-small.png'
+const largeCac = '/images/cactus-large.png'
+const bigCac = '/images/cactus-big.png'
+
 // Getting the instructions element
-const instrButton = document.getElementById('instructions')
-const instrContainer = document.getElementById('instruction-container')
+const instrButton = document.getElementById("instructions");
+const instrContainer = document.getElementById("instruction-container");
 
 // Getting the canvas element from html
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 // Getting the start/RAAAAWR button and the main men screen, as well as the game canvas
-const startButton = document.getElementById('start-game')
-const mainMenu = document.getElementById('border')
+const startButton = document.getElementById("start-game");
+const mainMenu = document.getElementById("border");
 
 // Mouseover/mouseout event listeners to make a kind of 'drop down' menu.
-instrButton.addEventListener('mouseover', () => {
-    instrContainer.style.display = 'block'
-})
-instrButton.addEventListener('mouseout', () => {
-    instrContainer.style.display = 'none'
-})
+instrButton.addEventListener("mouseover", () => {
+  instrContainer.style.display = "block";
+});
+instrButton.addEventListener("mouseout", () => {
+  instrContainer.style.display = "none";
+});
 
 // Mouseover/mouseout event listeners to make a kind of 'drop down' menu.
-instrButton.addEventListener('mouseover', () => {
-    instrContainer.style.display = 'block'
-})
-instrButton.addEventListener('mouseout', () => {
-    instrContainer.style.display = 'none'
-})
+instrButton.addEventListener("mouseover", () => {
+  instrContainer.style.display = "block";
+});
+instrButton.addEventListener("mouseout", () => {
+  instrContainer.style.display = "none";
+});
 
 // START THE GAME BUTTON
-startButton.addEventListener('click', () => {
-    mainMenu.style.display = 'none'
-    canvas.style.display = 'flex'
-})
+startButton.addEventListener("click", () => {
+  mainMenu.style.display = "none";
+  canvas.style.display = "flex";
+});
 
 // Updating the dino image (Creating a running animation)
 const dinoLeft = new Image();
@@ -39,17 +59,32 @@ dinoLeft.src = "./images/Dino-left.png";
 const dinoRight = new Image();
 dinoRight.src = "./images/Dino-right.png";
 
+
+const obstacles = [];
+function updateObstacles() {
+    for (i = 0; i < obstacles.length; i++) {
+        obstacles[i].x += -1;
+        obstacles[i].update();
+    }
+    if (frameCount % 480 === 0) {
+        let x = canvas.width
+        let height = 42;
+        let startingPosition = 260;
+        obstacles.push(new Obstacle(25, height, smallCac, x, startingPosition, 2))
+    }
+    requestAnimationFrame(updateObstacles);
+}
+
 // Dimensions and draw positions of the dinosaur, also defines the Y position of the ground.
 let dino = {
   x: 64,
   y: 240,
-  width: 40,
-  height: 40,
+  width: 64,
+  height: 64,
   speedY: 0,
   ground: 240,
   gravity: 0.25,
 };
-
 
 // sets the first image of the dino (with the right leg down) as well as the frameInterval(how fast it's running).
 let currentDinoImage = dinoRight;
@@ -74,10 +109,9 @@ function dinoRun() {
 // Dino jump mechanic.
 function dinoJump(e) {
   if (e.code == "Space" && dino.y === dino.ground) {
-    dino.speedY = -6;
+    dino.speedY = -7.5;
   }
 }
-
 
 function updateGame() {
   dino.speedY += dino.gravity;
@@ -99,8 +133,8 @@ function updateGame() {
 
 dinoRight.addEventListener("load", () => {
   updateGame();
+  updateObstacles()
 });
 
 // event listener that waits for the space button to get pressed, causing the dino to jump
 document.addEventListener("keydown", dinoJump);
-
